2837d9dc5d4fc951576f4661100202d9afe621e7
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2023-12-22 12:19:36 +0300

    final commit

---
+++ contact-list-app/src/App.js
@@ -0,0 +1,12 @@
+import React, { useState } from "react";
+import Example from "./Components/Example";
+
+function App() {
+  return (
+    <div className="App">
+    <Example/>
+    </div>
+  );
+}
+
+export default App;
